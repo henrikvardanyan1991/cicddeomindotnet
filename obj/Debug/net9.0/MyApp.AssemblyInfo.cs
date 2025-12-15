9a64716c377c6fa456d0c95b00466541b1aacb6c
henrikvardanyan1991 <henrikvardanyan1991@gmail.com>
2025-12-15 18:35:42 +0400

    forth commit

--- obj/Debug/net9.0/MyApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe61d078f9aaa2b968ceb57c88f6def94b01dc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f1e8ed8bb3ebb1400c7049dec634263945bbe3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
