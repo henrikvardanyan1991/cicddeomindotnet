aab0a6c7b8316c70f3356d9edb1425f88782f3d8
henrikvardanyan1991 <henrikvardanyan1991@gmail.com>
2025-12-17 13:30:05 +0400

    Add .gitignore

---
+++ MyApp.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace MyApp.Tests;
+
+public class UnitTest1
+{
+     [Fact]
+        public void Add_ShouldReturnSum()
+        {
+            var calc = new Calculator();
+            Assert.Equal(7, calc.Add(3, 4));
+        }
+
+        [Fact]
+        public void Subtract_ShouldReturnDifference()
+        {
+            var calc = new Calculator();
+            Assert.Equal(5, calc.Subtract(7, 2));
+        }
+}
---
+++ MyApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calculator();
+            Console.WriteLine($"3 + 4 = {calc.Add(3,4)}");
+            Console.WriteLine($"7 - 2 = {calc.Subtract(7,1)}");
+        }
+    }
+}
